75fe1cd6d56a9139353f21ee2212757f625d5799
mAtencio27 <mAtencio27@hotmail.com>
2023-01-29 19:23:32 +0900

    UPDATED: updated the tsconfig to compile es6. to use types on the resturants

--- src/components/step_2.tsx
+++ src/components/step_2.tsx
@@ -8,8 +8,44 @@ type Props = {
 }
 
 const Step_2: React.FC<Props> = ({ dishesData,setDishesData, orderInfo, setOrderInfo }) => {
+
+    let filterRestaurants = dishesData.filter((dish)=>{
+        for(let meals of dish.availableMeals){
+          if(meals === orderInfo.category){
+            return true
+          };
+        };
+      });
+    
+    let restaurantArray = [...new Set(filterRestaurants.map((r)=>{return r.restaurant}))];
+
+    let mappedRestaurants = restaurantArray.map((r) => {
+        return <option key={r} value={r}>{r}</option>
+      });
+
+    let changeHandler = (e:any) => {
+        //setOrderHolder([]);
+        setOrderInfo({
+          category: orderInfo.category,
+          numOfPeople: orderInfo.numOfPeople,
+          restaurant:e.target.value,
+          meals:[],
+        });
+      };
+
     return (
-        <div>step 2</div>
+        <div>
+            <div>
+                <label>
+                    <h3>Please Select a Resturant that offers {orderInfo.category}</h3>
+                    <h2>{orderInfo.restaurant} has been chosen</h2>
+                    <select value={orderInfo.restaurant} onChange={changeHandler}>
+                        <option value = "">---</option>
+                        {mappedRestaurants}
+                    </select>
+                </label>
+            </div>
+        </div>
     )
 };
 
