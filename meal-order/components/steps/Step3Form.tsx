2102a628cad9c36addd51f6380ded822ce0024da
Enreina <enreina@gmail.com>
2023-01-31 17:52:40 +0700

    Render total number of servings

--- meal-order/components/steps/Step3Form.tsx
+++ meal-order/components/steps/Step3Form.tsx
@@ -9,14 +9,21 @@ export default function Step3Form({
   selectedDishes: SelectedDishes;
   onUpdateDish: (dish: Dish, numberOfServing: number) => void;
 }) {
+  const totalNumServings = Object.values(selectedDishes).reduce(
+    (prevValue, { numberOfServing }) => {
+      return prevValue + numberOfServing;
+    },
+    0
+  );
+
   return (
     <ul className="flex flex-col justify-center mt-4">
       {availableDishes.map((dish) => {
         return (
-          <li key={dish.id} className="flex flex-row justify-between">
+          <li key={dish.id} className="flex flex-row justify-between mt-4">
             <span className="p-2">{dish.name}</span>
             <input
-              className="border border-solid border-blue-400 rounded text-center p-2"
+              className="border border-solid border-blue-400 rounded-xl text-center p-2"
               type="number"
               min="0"
               max="10"
@@ -36,6 +43,10 @@ export default function Step3Form({
           </li>
         );
       })}
+      <li className="flex flex-row justify-between border border-solid border-black mt-4 rounded-lg">
+        <span className="p-2 font-bold">Total servings:</span>
+        <span className="p-2 font-bold">{totalNumServings}</span>
+      </li>
     </ul>
   );
 }
