a222f0bcf7b399e6d68aa098728b9268aa3911d0
33YANG <yangyata@foxmail.com>
2023-02-03 16:15:22 +0800

    chore: add github deploy pages folder

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<33YANG>/<react_assignment>.git main:gh-pages
+git push -f git@github.com:33YANG/react_assignment.git main:gh-pages
 
 cd -
